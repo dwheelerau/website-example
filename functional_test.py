afacd393ec47a5b732373cca36ddaaa58bc31161
Dave Wheeler <dwheelerau@gmail.com>
2014-07-03 16:23:18 +1200

    added test class

--- functional_test.py
+++ functional_test.py
@@ -1,31 +1,44 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase): #1
 
-#fred has heard about a cool list site on the web
-#he decides to go out and check it out
-browser.get('http://localhost:8000')
+    def setUp(self): #2
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-#he notice the page title and header mention a to-do list
-assert 'To-Do' in browser.title
+    def tearDown(self): #3
+        self.browser.quit()
 
-#he is invited to enter a to-do item straigt away
+    def test_can_start_a_list_and_retrive_it_later(self): #4
+        #fred has heard about a cool list site on the web
+        #he decides to go out and check it out
+        self.browser.get('http://localhost:8000')
 
-#he types "buy peacock feathers" into a text box (for fly fishing
-#lours)
+        #he notice the page title and header mention a to-do list
+        self.assertIn('To-Do',self.browser.title) #5
+        self.fail('Finish the test')#6
 
-#when he hits enter the page updates and now the pages lists
-# "1: buy peacock feathers" as a todo list item
+        #he is invited to enter a to-do item straigt away
 
-#there is still a text box inviting him to add another item.
-#he enters "use feathers to make fly"
+        #he types "buy peacock feathers" into a text box (for fly fishing
+        #lours)
 
-#the page updates again, now both her list items are there
+        #when he hits enter the page updates and now the pages lists
+        # "1: buy peacock feathers" as a todo list item
 
-#fred wonders what will become of his list, he notes that the 
-#url has been created that is unique to this page, and that
-#there is some explanation that says something to that effect
+        #there is still a text box inviting him to add another item.
+        #he enters "use feathers to make fly"
 
-#he ivsit that url - her to do list is still there
+        #the page updates again, now both her list items are there
 
-#satisfied he turns of the computer and gets a life
+        #fred wonders what will become of his list, he notes that the 
+        #url has been created that is unique to this page, and that
+        #there is some explanation that says something to that effect
+
+        #he ivsit that url - her to do list is still there
+
+        #satisfied he turns of the computer and gets a life
+
+if __name__ == '__main__': #7
+    unittest.main(warnings='ignore') #8
